6669fb9723cb793410bc3df6a88c3c6f4fc116ec
Khoa Vu Dang <khoavd@fds.vn>
2017-08-02 09:53:26 +0700

    Add XSD file and Interface for DocFile

---
+++ src/main/java/org/mobilink/resource/DocFile.java
@@ -0,0 +1,11 @@
+package org.mobilink.resource;
+
+import javax.ws.rs.Path;
+
+import io.swagger.annotations.Api;
+
+@Path("/docfiles")
+@Api(value="/docfiles", description="APIs for DocFile", tags="doc,file,docfiles")
+public interface DocFile {
+	
+}
